c66623f21b42ef7403664f409ec9047f1ede4d9f
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-02-14 14:44:59 +0530

    Edit Articles, Show All Articles, Links to navigate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 # Articles Controller
 class ArticlesController < ApplicationController
 
+	# New Article
 	def new 
 		@article = Article.new
 	end
 
+	# Save an article and show the article to the user.	
 	def create
 		# Able to see whats being displayed in the browser
 		# render plain: params[:article].inspect
@@ -22,7 +24,6 @@ class ArticlesController < ApplicationController
 		else
 			render 'new'
 		end
-
 		# redirect to show method in the article
 		# redirect_to article_path(@article)
 	end
@@ -31,13 +32,30 @@ class ArticlesController < ApplicationController
 			@article = Article.find(params[:id])
 	end
 
+	# Edit an article and update action
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+	# Displays the articles to the user	
+	def index
+		@articles = Article.all
+	end
+
+
+
 	private
 	def article_params
 		# It will take the fields from the render part above and fetch title and description values
 		params.require(:article).permit(:title, :description)
 	end
-
-
-
-
 end
