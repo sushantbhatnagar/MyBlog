69d23e3b3979844e3b5b262ef7e55ddf137c707a
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-02-14 13:29:12 +0530

    Create New Article, Show the saved article and validations, added link to navigate to New Article page from Home Page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,16 +6,34 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		# @article.save!
+		# Able to see whats being displayed in the browser
 		# render plain: params[:article].inspect
+
+		# Created a new article instance variable passing the another method
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_show(@article)
+
+		# save the article
+		# @article.save
+
+		# putting in some validations
+		if @article.save
+			# flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+
+		# redirect to show method in the article
+		# redirect_to article_path(@article)
 	end
 
+	def show
+			@article = Article.find(params[:id])
+	end
 
 	private
 	def article_params
+		# It will take the fields from the render part above and fetch title and description values
 		params.require(:article).permit(:title, :description)
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
 
-  get 'pages/home'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  	get 'pages/home'
+	
 	# get 'pages/home', to: 'pages#home'
 	root 'pages#home'
 	get 'about', to: 'pages#about'
-  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	
 
-  	# This will give new, edit , patch and get article path
+  	# This will give new, edit , patch and get article path. To check, rails routes is the command
   	resources :articles
 
-  	
+
 
 
 
